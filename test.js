65870e7c4345abbc0e2452dadce82717dc642e60
Bran van der Meer <bran.van.der.meer@protonmail.com>
2022-04-27 19:29:15 +0200

    feat: Initialise project with CRA

---
+++ test.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAY0NnZNi8YwcoOl-QXx4AdfGjWl2EHPWQ',
+  authDomain: 'recipe-manager-5000.firebaseapp.com',
+  projectId: 'recipe-manager-5000',
+  storageBucket: 'recipe-manager-5000.appspot.com',
+  messagingSenderId: '350783515773',
+  appId: '1:350783515773:web:6062d6234a7de52ae81514',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
