2f57f9b68f6ca9aa9ce8f0e329cd7023e55ea33a
Bran van der Meer <bran.van.der.meer@protonmail.com>
2022-05-06 16:11:16 +0200

    feat: Add user greeting by name

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,17 +1,24 @@
-import { auth } from '../../util/firebase'
+import { auth, db } from '../../util/firebase'
 import { signOut } from 'firebase/auth'
+import { ref } from 'firebase/database'
 import { useAuthState } from 'react-firebase-hooks/auth'
+import { useObjectVal } from 'react-firebase-hooks/database'
 
 import Button from '@mui/material/Button'
 import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
 
 export default function Header() {
   const [user] = useAuthState(auth)
+  const r = ref(db, 'users/' + user.uid)
+  const [userData, loading, error] = useObjectVal(r)
 
   return (
     <Grid container spacing={2} alignItems='center'>
       <Grid item xs={8}>
-        <p>Logged in as: {user.email}</p>
+        {!error && !loading && userData && (
+          <Typography>Welcome {userData.name}!</Typography>
+        )}
       </Grid>
       <Grid item xs={4} sx={{ textAlign: 'right' }}>
         <Button variant='outlined' onClick={() => signOut(auth)}>
